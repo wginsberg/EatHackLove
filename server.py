4eb41962bf5a5c7209d7a92e8a02d07189dff91f
Will Ginsberg <will.j.ginsberg@gmail.com>
2018-02-17 13:39:27 -0500

    Prettify shopping cart price

--- server.py
+++ server.py
@@ -22,6 +22,7 @@ def calculate_test():
     data = [{   
                 'store': 'MTR',
                 'total': 12.99,
+                'color': "green",
                 'basket': [
                         {'name': 'oatmeal', 'serving': ' 2 x 100g'},
                         {'name': 'olive oil', 'serving': ' 1 x 15ml'}
@@ -30,6 +31,7 @@ def calculate_test():
             {
                 'store': 'NF',
                 'total': 14.25,
+                'color': 'black',
                 'basket': [
                         {'name': 'oatmeal', 'serving': ' 2 x 100g'},
                         {'name': 'olive oil', 'serving': ' 1 x 15ml'}
@@ -38,6 +40,7 @@ def calculate_test():
             {
                 'store': 'LBW',
                 'total': 14.99,
+                'color': 'black',
                 'basket': [
                         {'name': 'oatmeal', 'serving': ' 2 x 100g'},
                         {'name': 'olive oil', 'serving': ' 1 x 15ml'}
@@ -59,4 +62,17 @@ def calculate():
             ">" + input['protein']
             ))
     data = budget_calc.main("food-db", "bulking-a")
+    
+    # Set basket colour
+    best_total = data[0]['total']
+    best_i = 0
+    for i, calc in enumerate(data):
+        data[i]['color'] = 'black'
+        if calc['total'] < best_total:
+            best_total = calc['total']
+            best_i = i
+
+    data[best_i]['color'] = 'green'
+
     return render_template('final_page.jinja', data=data)
+
